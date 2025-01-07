3d1e4a943c986d40fe0ab5b211ca386bf16fe3ee
d999ss <d999ss@gmail.com>
2025-01-06 17:11:59 -0700

    Update frontend configuration with production backend URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_URL: process.env.NODE_ENV === 'production'
-    ? 'https://robotrader-api.onrender.com'  // We'll deploy the backend to Render
-    : 'http://localhost:5002'
+    ? 'https://robotrader-api.onrender.com'  // Production backend URL
+    : 'http://localhost:5002'  // Development backend URL
 };
 
 export default config;
